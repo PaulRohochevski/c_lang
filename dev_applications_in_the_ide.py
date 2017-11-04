9f7aa35f772c00c714dfea79e5ba7b33eaf3deb2
prahacheuski <paul.rohochevski@gmail.com>
2017-11-04 22:52:30 +0300

    university assignment

--- dev_applications_in_the_ide.py
+++ dev_applications_in_the_ide.py
@@ -1,4 +1,30 @@
 """
+lab #1 task #21
+Вывести на экран 1 или 0 в зависимости от того, есть ли среди первых трех цифр дробной части заданного положительного
+вещественного числа цифра нуль.
+"""
+
+
+def zero_detector(real_num: float) -> int:
+    real_str = str(real_num)
+    dot = real_str.find('.')
+    after_dot = real_str[dot + 1:]
+    char_index = 0
+    for char in after_dot:
+        char_index += 1
+        if (char == '0') and (char_index < 4):
+            print('Detected zero in real between 1 and 3 position after dot: {}'.format(real_str))
+            return 1
+        elif char_index > 3:
+            print('No zero in real between 1 and 3 position after dot: {}'.format(real_str))
+            return 0
+
+
+zero_detector(35.16907)
+zero_detector(192.98097)
+zero_detector(0.356790)
+
+"""
 lab #2 task #21
 В старояпонском календаре был принят 60-летний цикл, состоявший из пяти 12-летних подциклов. Подциклы обозначались 
 названиями цвета:  green (зеленый), red (красный), yellow (желтый), white (белый), black (черный). Внутри каждого 
@@ -38,3 +64,35 @@ class JapanCalendar(object):
 
 
 JapanCalendar(2017).japan_calendar()
+JapanCalendar(1017).japan_calendar()
+JapanCalendar(3017).japan_calendar()
+
+"""
+lab #3 task #21
+Дано n вещественных чисел. Определить, образуют ли они возрастающую последовательность.
+"""
+
+sample_1 = (12.3, 15.3, 35.4, 45.7, -16.9, -23.4)
+sample_2 = (-16.9, -23.4, 35.4, 12.3, 45.7, 15.3)
+sample_3 = (-23.4, -16.9, 12.3, 15.3, 35.4, 45.7)
+
+
+def sequence(sample: tuple) -> int:
+    increased = True
+    for i in range(1, len(sample)):
+        if sample[i - 1] < sample[i]:
+            continue
+        else:
+            increased = False
+
+    if increased:
+        print('{} is increasing sequence'.format(sample))
+        return 0
+    elif not increased:
+        print('{} is not increasing sequence'.format(sample))
+        return 0
+
+
+sequence(sample_1)
+sequence(sample_2)
+sequence(sample_3)
