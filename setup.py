371603469b9ce2a83a6d1c5e72ad3bfccf71f17c
prahacheuski <paul.rohochevski@gmail.com>
2018-11-28 22:15:40 +0300

    Added 'requires' parameter.

--- setup.py
+++ setup.py
@@ -6,4 +6,4 @@ Build command: python setup.py build_ext --inplace
 
 ext_options = {"annotate": True}
 
-setup(name='calc', ext_modules=cythonize('calc.pyx', **ext_options))
+setup(name='calc', ext_modules=cythonize('calc.pyx', **ext_options), requires=['pandas', 'Cython', 'numpy', 'PyQt5'])
